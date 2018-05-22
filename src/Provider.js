04f3b3a0aca43f6f198bf9b12e6266e9458f69cb
Zephraph <zephraph@gmail.com>
2018-05-22 00:33:21 -0500

    fix(Provider): Unwrap from provider object, don't overwrite context value with undefined prop

--- src/Provider.js
+++ src/Provider.js
@@ -1,22 +1,22 @@
 export default function Provider(context, defaultValue) {
   return {
-    Provider: {
-      props: {
-        value: {
-          default: () => defaultValue
-        }
-      },
-      created() {
-        context.value = this.value;
-      },
-      watch: {
-        value(v) {
-          context.value = v;
-        }
-      },
-      render(h) {
-        return this.$slots.default[0];
+    props: {
+      value: {
+        default: () => defaultValue
       }
+    },
+    created() {
+      if (this.value !== undefined) {
+        context.value = this.value;
+      }
+    },
+    watch: {
+      value(v) {
+        context.value = v;
+      }
+    },
+    render(h) {
+      return this.$slots.default[0];
     }
   };
 }
