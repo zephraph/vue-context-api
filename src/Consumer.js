c89a8d1ee4cf2141844420a19350fb01b4f049f0
Zephraph <zephraph@gmail.com>
2018-05-22 00:31:50 -0500

    fix(Consumer): Render child content when slot-scope isn't defined. Provide a warning.
    If slot slot-scoped isn't defined then this.$scopedSlots.default will be undefined. Calling in in
    that scenario would result in an error. If it's undefined, fall back to render the default slot
    while giving an error that slot-scope should be defined

--- src/Consumer.js
+++ src/Consumer.js
@@ -4,6 +4,10 @@ export default function Consumer(context) {
       context
     }),
     render(h) {
+      if (!this.$scopedSlots.default) {
+        console.warn("Context consumer expected to define slot-scope");
+        return this.$slots.default[0];
+      }
       return this.$scopedSlots.default(this.context.value);
     }
   };
